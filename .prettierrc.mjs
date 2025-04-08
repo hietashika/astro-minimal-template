17981132fa1835110e2ba2e8583b40793cad4e38
hietashika <hietashika@gmail.com>
2025-04-08 17:45:45 +0900

    [chore] 🔧 設定ファイルの整形を行う - 設定ファイルのフォーマットを統一し、可読性を向上させました。

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,14 +3,12 @@ const require = createRequire(import.meta.url);
 
 /** @type {import("prettier").Config} */
 export default {
-	plugins: [
-		require.resolve("prettier-plugin-astro"),
-	],
-	useTabs: false,
-	tabWidth: 2,
-	semi: true,
-	singleQuote: false,
-	trailingComma: "all",
-	printWidth: 80,
-	endOfLine: "crlf",
+  plugins: [require.resolve("prettier-plugin-astro")],
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  endOfLine: "crlf",
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-  ],
+  integrations: [],
   experimental: {
     svg: true,
   },
